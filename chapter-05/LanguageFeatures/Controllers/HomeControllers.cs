9e62070fa85c8ce06fee65ce010247b881f71622
ntlam3 <nguyentunglam226@gmail.com>
2020-09-10 11:17:27 +0700

    Chapter 5 - Add code for Using Object and Collection Initializers

--- chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
+++ chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
@@ -7,15 +7,17 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            List<string> results = new List<string>();
-            foreach (Product p in Product.GetProducts())
-            {
-                string name = p?.Name ?? "<No Name>";
-                decimal? price = p?.Price ?? 0;
-                string relatedName = p?.Related?.Name?? "<None>";
-                results.Add(string.Format($"Name: {name}, Price: {price:C2}, Related {relatedName}"));
-            }
-            return View(results);
+            // List<string> results = new List<string>();
+            // foreach (Product p in Product.GetProducts())
+            // {
+            //     string name = p?.Name ?? "<No Name>";
+            //     decimal? price = p?.Price ?? 0;
+            //     string relatedName = p?.Related?.Name?? "<None>";
+            //     results.Add(string.Format($"Name: {name}, Price: {price:C2}, Related {relatedName}"));
+            // }
+            // return View(results);
+
+            return View("Index", new string[] { "Bob", "Joe", "Alice" });
         }
     }
 }

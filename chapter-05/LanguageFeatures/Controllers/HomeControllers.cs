a702fb48a5597afe5ece42d9cf2548188853aaaf
finalaurora <ntlisme@hotmail.com>
2020-10-19 01:34:57 +0700

    Chapter 05: Creating Filtering Extension Methods

--- chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
+++ chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
@@ -8,14 +8,16 @@ namespace LanguageFeatures.Controllers
         public ViewResult Index()
         {
             ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
-            Product[] productArray = 
+            Product[] productArray =
             {
                 new Product{Name = "Kayak", Price= 275M},
-                new Product{Name = "Lifejacket", Price= 48.95M}
+                new Product{Name = "Lifejacket", Price= 48.95M},
+                new Product{Name = "SoccerBall", Price=19.50M},
+                new Product{Name = "Corner Flag", Price= 34.95M}
             };
 
             decimal cartTotal = cart.TotalPrices();
-            decimal arrayTotal = productArray.TotalPrices();
+            decimal arrayTotal = productArray.FilterByPrice(20).TotalPrices();
 
             return View("Index", new string[] { $"Total: {cartTotal:C2}",
             $"Array Total: {arrayTotal:C2}" });
--- chapter-05/LanguageFeatures/Models/MyExtensionMethods.cs
+++ chapter-05/LanguageFeatures/Models/MyExtensionMethods.cs
@@ -12,5 +12,16 @@ namespace LanguageFeatures.Models
             }
             return total;
         }
+
+        public static IEnumerable<Product> FilterByPrice(this IEnumerable<Product> productEnum, decimal minimumPrice)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if ((prod?.Price ?? 0) >= minimumPrice)
+                {
+                    yield return prod;
+                }
+            }
+        }
     }
 }

8f46a4ab8442a543389215191f6657f691acac16
finalaurora <ntlisme@hotmail.com>
2020-10-20 00:58:00 +0700

    Chapter05: Using Lambda Expression Methods and Properties

--- chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
+++ chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
@@ -2,37 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using LanguageFeatures.Models;
 using System;
+using System.Linq;
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        bool FilterByPrice(Product p)
-        {
-            return (p?.Price ?? 0)>=20;
-        }
-        public ViewResult Index()
-        {
-            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
-            Product[] productArray =
-            {
-                new Product{Name = "Kayak", Price= 275M},
-                new Product{Name = "Lifejacket", Price= 48.95M},
-                new Product{Name = "SoccerBall", Price=19.50M},
-                new Product{Name = "Corner Flag", Price= 34.95M}
-            };
+        public ViewResult Index() =>
 
-            Func<Product, bool> namefilter = delegate (Product prod)
-            {
-                return prod?.Name?[0] == 'S';
-            };
-
-            decimal priceFilterTotal = productArray.Filter(p => (p?.Price ?? 0)>=20 ).TotalPrices();
-            decimal nameFilterTotal = productArray.Filter(p => p?.Name?[0]=='S').TotalPrices();
-
-            return View("Index", new string[] { $"Name Total: {nameFilterTotal:C2}",
-            $"Price Total: {priceFilterTotal:C2}" });
-        }
-
+            View(Product.GetProducts().Select(p => p?.Name));
     }
 
 

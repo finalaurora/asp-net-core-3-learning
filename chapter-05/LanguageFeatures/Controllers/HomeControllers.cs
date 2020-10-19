c6299c5a33ac4d47aee9a93b81ce8f146b2ec42e
finalaurora <ntlisme@hotmail.com>
2020-10-20 01:09:00 +0700

    Chapter05: Using Anonymous Types

--- chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
+++ chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
@@ -7,9 +7,17 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() =>
+        public ViewResult Index()
+        {
+            var products = new[]{
+               new { Name = "Kayak", Price = 275M },
+               new { Name = "Lifejacket", Price = 48.95M },
+               new { Name = "Soccer ball", Price = 19.50M },
+               new { Name = "Corner flag", Price = 34.95M }
+            };
 
-            View(Product.GetProducts().Select(p => p?.Name));
+            return View(products.Select(p => p.Name));
+        }
     }
 
 

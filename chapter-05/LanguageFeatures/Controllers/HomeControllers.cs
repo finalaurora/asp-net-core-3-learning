de3fc61f4359ad6836d845bc61cd1a66c49cb2e1
finalaurora <ntlisme@hotmail.com>
2020-10-20 01:57:07 +0700

    Chapter 05: Getting Names

--- chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
+++ chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
@@ -8,15 +8,15 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<ViewResult> Index()
+        public ViewResult Index()
         {
-            List<string> output = new List<string>();
-            await foreach (long? len in MyAsyncMethods.GetPageLengths(output,
-            "apress.com", "microsoft.com", "amazon.com"))
-            {
-                output.Add($"Page length: { len}");
-            }
-            return View(output);
+            var products = new[] {
+                new { Name = "Kayak", Price = 275M },
+                new { Name = "Lifejacket", Price = 48.95M },
+                new { Name = "Soccer ball", Price = 19.50M },
+                new { Name = "Corner flag", Price = 34.95M }
+                };
+            return View(products.Select(p => $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
         }
     }
 

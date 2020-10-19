4474879f0ece74ffcbe2d93fe5b620f32d45cc18
finalaurora <ntlisme@hotmail.com>
2020-10-20 01:48:42 +0700

    Chapter05: Using an Asynchronous Enumerable 1

--- chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
+++ chapter-05/LanguageFeatures/Controllers/HomeControllers.cs
@@ -10,8 +10,13 @@ namespace LanguageFeatures.Controllers
     {
         public async Task<ViewResult> Index()
         {
-            long? length = await MyAsyncMethods.GetPageLength();
-            return View(new string[] {$"Length: {length}"});
+            List<string> output = new List<string>();
+            foreach (long? len in await MyAsyncMethods.GetPageLengths(output,
+            "apress.com", "microsoft.com", "amazon.com"))
+            {
+                output.Add($"Page length: { len}");
+            }
+            return View(output);
         }
     }
 
--- chapter-05/LanguageFeatures/Models/MyAsyncMethods.cs
+++ chapter-05/LanguageFeatures/Models/MyAsyncMethods.cs
@@ -1,5 +1,6 @@
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 namespace LanguageFeatures.Models
 {
     public class MyAsyncMethods
@@ -9,6 +10,20 @@ namespace LanguageFeatures.Models
             HttpClient client = new HttpClient();
             var httpMessage = await client.GetAsync("http://apress.com");
             return httpMessage.Content.Headers.ContentLength;
+        }
+
+        public static async Task<IEnumerable<long?>> GetPageLengths(List<string> output, params string[] urls)
+        {
+            List<long?> results = new List<long?>();
+            HttpClient client = new HttpClient();
+            foreach (string url in urls)
+            {
+                output.Add($"Started request for {url}");
+                var httpMessage = await client.GetAsync($"http://{url}");
+                results.Add(httpMessage.Content.Headers.ContentLength);
+                output.Add($"Completed request for {url}");
+            }
+            return results;
         }
     }
 }

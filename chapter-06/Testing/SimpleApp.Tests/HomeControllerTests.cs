0b54f1ac981c068a2a87e69ba9319782f046a78c
finalaurora <ntlisme@hotmail.com>
2020-10-24 02:46:26 +0700

    Chapter 06: Using a Mocking Package

--- chapter-06/Testing/SimpleApp.Tests/HomeControllerTests.cs
+++ chapter-06/Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using SimpleApp.Controllers;
 using SimpleApp.Models;
 using Xunit;
+using Moq;
 
 namespace SimpleApp.Tests
 {
     public class HomeControllerTests
     {
-        class FakeDataSource : IDataSource
-        {
-            public FakeDataSource(Product[] data) => Products = data;
-            public IEnumerable<Product> Products {get; set;}
-        }
+        // class FakeDataSource : IDataSource
+        // {
+        //     public FakeDataSource(Product[] data) => Products = data;
+        //     public IEnumerable<Product> Products {get; set;}
+        // }
 
         [Fact]
         public void IndexActionModelIsComplete(){
@@ -26,13 +27,15 @@ namespace SimpleApp.Tests
                 new Product{Name = "P3", Price = 100M}
             };
 
-            FakeDataSource data = new FakeDataSource(testData);
-            controller.dataSource=data;
+            var mock = new Mock<IDataSource>();
+            mock.SetupGet(m => m.Products).Returns(testData);
+            controller.dataSource=mock.Object;
             //Act
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
             //  Assert
-            Assert.Equal(data.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+            Assert.Equal(testData, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+            mock.VerifyGet(m => m.Products, Times.Once);
         }
     }
 }

49e3bc6ea58c27b825e22fd665251fc1c5055248
finalaurora <ntlisme@hotmail.com>
2020-10-24 01:48:05 +0700

    Chapter 06: Writing and Running Unit Tests - 2

--- chapter-06/Testing/SimpleApp.Tests/ProductTests.cs
+++ chapter-06/Testing/SimpleApp.Tests/ProductTests.cs
@@ -16,5 +16,17 @@ namespace SimpleApp.Tests
             // Assert
             Assert.Equal("New Name", p.Name);
         }
+
+        [Fact]
+        public void CanChangeProductPrice(){
+            // Arrange
+            var p = new Product{Name = "Test", Price = 100M};
+
+            // Act
+            p.Price= 200M;
+
+            // Assert
+            Assert.Equal(200M, p.Price);
+        }
     }
 }
